18b13ce4046c485eca80c2448a9dc62c5ef9c9f6
unknown <siammdarafathislam@gmail.com>
2023-11-10 00:18:36 +0600

    handle form submission

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -4,7 +4,7 @@ import prisma from "@/prisma/client";
 
 const createIssueSchema = z.object({
   title: z.string().min(1).max(255),
-  descrtiption: z.string().min(1),
+  description: z.string().min(1),
 });
 
 export async function POST(request: NextRequest) {
@@ -16,5 +16,6 @@ export async function POST(request: NextRequest) {
   const newIssue = await prisma.issue.create({
     data: { title: body.title, description: body.description },
   });
+
   return NextResponse.json(newIssue, { status: 201 });
 }
